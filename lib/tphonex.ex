300fb84101e236ba91bee4031d78c92cf84636cb
Roman Sotnikov <retgoat@gmail.com>
2025-11-14 14:51:31 +0700

    Initial
    Signed-off-by: Roman Sotnikov <retgoat@gmail.com>

---
+++ lib/tphonex.ex
@@ -0,0 +1,18 @@
+defmodule Tphonex do
+  @moduledoc """
+  Documentation for `Tphonex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tphonex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tphonex_test.exs
@@ -0,0 +1,8 @@
+defmodule TphonexTest do
+  use ExUnit.Case
+  doctest Tphonex
+
+  test "greets the world" do
+    assert Tphonex.hello() == :world
+  end
+end
